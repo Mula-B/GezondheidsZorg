341ab9f2b6cb68205cbf48f496c49d90d6f42ce9
Niborm <36726525+Niborm@users.noreply.github.com>
2018-06-20 11:54:33 +0200

    fsd

---
+++ GezondheidsZorg/Verzekering.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GezondheidsZorg
+{
+    class Verzekering
+    {
+        public int VerzekeringID { get; set; }
+        public string VerzekeringNaam { get;set }
+    }
+}
